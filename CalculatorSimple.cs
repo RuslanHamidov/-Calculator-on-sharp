91ff9c8506e250138a0e308a92882527aea0b08a
RuslanHamidov <124673207+RuslanHamidov@users.noreply.github.com>
2023-04-26 23:03:42 +0400

    Add files via upload

---
+++ CalculatorSimple.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+Console.WriteLine("Enter first number: ");
+float a = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+float b = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Select operation:\n1.Sum(sum)\n2.Extraction(ex)\n3.Multiplying(mult)\n4.Division(div)\n");
+string operation = (Console.ReadLine());
+
+if (operation == "sum")
+{
+
+    Console.WriteLine(a + b);
+}
+
+if (operation == "ex")
+{
+
+    Console.WriteLine(a - b);
+}
+
+
+if (operation == "mult")
+{
+
+    Console.WriteLine(a * b);
+}
+
+if (operation == "div")
+{
+
+    Console.WriteLine(a / b);
+}
